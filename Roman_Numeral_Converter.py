032572994a72a8892e07a31b7d4d80b096036d99
Kooreis <kooreis.official@gmail.com>
2023-09-09 00:09:15 -0500

    This update includes the function 'int_to_roman' which converts an integer to a Roman numeral. It checks if the input is an integer, then uses a while loop to convert the integer to a Roman numeral. If the input is not an integer, it returns 'Invalid input'.

--- Roman_Numeral_Converter.py
+++ Roman_Numeral_Converter.py
@@ -16,32 +16,3 @@ def int_to_roman(input):
         return roman_num
     else:
         return "Invalid input"
-
-def roman_to_int(input):
-    if isinstance(input, type("I")):
-        rom_val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        int_val = 0
-        for i in range(len(input)):
-            if i > 0 and rom_val[input[i]] > rom_val[input[i - 1]]:
-                int_val += rom_val[input[i]] - 2 * rom_val[input[i - 1]]
-            else:
-                int_val += rom_val[input[i]]
-        return int_val
-    else:
-        return "Invalid input"
-
-while True:
-    print("\n1. Integer to Roman Numeral")
-    print("2. Roman Numeral to Integer")
-    print("3. Exit")
-    choice = input("Enter your choice: ")
-    if choice == '1':
-        num = int(input("Enter an integer: "))
-        print(int_to_roman(num))
-    elif choice == '2':
-        num = input("Enter a Roman Numeral: ")
-        print(roman_to_int(num.upper()))
-    elif choice == '3':
-        break
-    else:
-        print("Invalid choice")
