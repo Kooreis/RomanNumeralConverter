84446d6fd7200dcae9948b0902fadda52db82fe4
Kooreis <kooreis.official@gmail.com>
2023-09-10 00:09:15 -0500

    Created the main program class and method. This prompts the user to enter a number and then converts that number to a Roman numeral.

--- Roman_Numeral_Converter.cs
+++ Roman_Numeral_Converter.cs
@@ -1,2 +1,8 @@
-using System;
-using System.Collections.Generic;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number to convert to Roman Numerals:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Roman Numeral: " + IntToRoman(number));
+    }
